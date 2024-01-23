1236f6257614835864828c4b1f2dc01de558d4f6
Gulshan Kumar <gulshangk1134@gmail.com>
2024-01-24 00:49:49 +0530

    create a test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test =(req,res)=>{
+    res.json({
+        message:"Hello Gulshan! API route is working fine!",
+    });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +16,8 @@ app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
     }
 );
+
+app.use("/api/user",userRouter);
 
 
 
