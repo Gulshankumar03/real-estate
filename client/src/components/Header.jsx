e0ac0a95bac5a6807721e6cc4e8dc7db013f0043
Gulshan Kumar <gulshangk1134@gmail.com>
2024-01-24 15:45:23 +0530

    complete signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,11 +2,11 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-full mx-auto p-3">
+    <header className="backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-slate-300/75">
+      <div className="flex justify-between items-center max-w-full  mx-auto py-3 px-8">
         <Link to={"/"}>
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Real</span>
+            <span className="text-slate-600">Real</span>
             <span className="text-slate-900">Estate</span>
           </h1>
         </Link>
@@ -20,17 +20,17 @@ export default function Header() {
         </form>
         <ul className="flex gap-8">
           <Link to={"/"}>
-            <li className="hidden sm:inline text-slate-800 hover:underline">
+            <li className="hidden sm:inline text-slate-800 font-medium hover:text-violet-500">
               Home
             </li>
           </Link>
           <Link to={"/about"}>
-            <li className="hidden sm:inline text-slate-800 hover:underline">
+            <li className="hidden sm:inline text-slate-800 font-medium hover:text-violet-500">
               About
             </li>
           </Link>
           <Link to={"/sign-in"}>
-            <li className="hidden sm:inline text-slate-800 hover:underline">
+            <li className="sm:inline text-slate-800 font-medium hover:text-violet-500">
               Sign in
             </li>
           </Link>
